f0cb716b1cb107fbca37629cce7987cbdb9bd944
Alexandru Grivei <fratiigrimm.dev@gmail.com>
2021-12-13 16:28:40 +0200

    rename files

--- 2_IDSUP_GRIVEI_Alexandru.js
+++ 2_IDSUP_GRIVEI_Alexandru.js
@@ -1,3 +1,38 @@
+var countries = {
+    AA: 'select a country',
+    BE: 'Belgium',
+    BG: 'Bulgaria',
+    CZ: 'Czechia',
+    DK: 'Denmark',
+    DE: 'Germany',
+    EE: 'Estonia',
+    IE: 'Ireland',
+    EL: 'Greece',
+    ES: 'Spain',
+    FR: 'France',
+    HR: 'Croatia',
+    IT: 'Italy',
+    CY: 'Cyprus',
+    LV: 'Latvia',
+    LT: 'Lithuania',
+    LU: 'Luxembourg',
+    HU: 'Hungary',
+    MT: 'Malta',
+    NL: 'Netherlands',
+    AT: 'Austria',
+    PL: 'Poland',
+    PT: 'Portugal',
+    RO: 'Romania',
+    SI: 'Slovenia',
+    SK: 'Slovakia',
+    FI: 'Finland',
+    SE: 'Sweden'
+};
+let select = document.getElementById("countries");
+for (index in countries) {
+    select.options[select.options.length] = new Option(countries[index], index);
+}
+
 class BarChart {
     #svgns;
     #domElement;
